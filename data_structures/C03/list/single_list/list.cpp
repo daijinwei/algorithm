6a27175a54436809e625aaf10abbafc420ed1d24
daijinwei <daijinwei41@gmail.com>
2015-03-04 23:27:32 +0800

    Modify single list

--- data_structures/C03/list/single_list/list.cpp
+++ data_structures/C03/list/single_list/list.cpp
@@ -3,16 +3,43 @@
 #include "list.h"
 
 /*Return true if L is empty*/
-bool is_empty(List L){
+bool list_is_empty(List L){
     return L->next == NULL;
 }
 
 /*  return true if p is the last position in list L */
-bool is_last(List L, Position p){
+bool list_is_last(List L, Position p){
     return p->next == NULL;
 }
 
-Position find_element(List L, ElementType value){
+void list_create_form_head(List L, ElementType array[], int n){
+	int i = 0;
+    Position tmp_node;
+    L = (Position)malloc(sizeof(struct Node));
+	L->next = NULL;
+	for(i; i < n; ++i){
+        tmp_node = (Position)malloc(sizeof(struct Node));
+        tmp_node->element = array[i];
+        tmp_node->next = L->next;
+        L->next = tmp_node;
+    }
+}
+
+void list_create_form_tail(List L, ElementType array[], int n){
+	int i = 0;
+    Position tail_previous, tmp_node;
+    L = (Position)malloc(sizeof(struct Node));
+	L->next = NULL;
+    tail_previous = L;
+	for(i; i < n; ++i){
+        tmp_node = (Position)malloc(sizeof(struct Node));
+        tmp_node->element = array[i];
+		tail_previous->next = tmp_node;
+		tail_previous = tmp_node;
+    }
+}
+
+Position list_find_element(List L, ElementType value){
     Position tmp;
     tmp = L->next;
     while(tmp != NULL && (tmp->element == value)) {
@@ -20,27 +47,27 @@ Position find_element(List L, ElementType value){
     }
 }
 
-Position find_previous(List L, ElementType value){
+Position list_find_previous(List L, ElementType value){
     Position tmp;
     tmp = L;
-    while(L->next != NULL && tmp->next->element != value){
+    while(tmp->next != NULL && tmp->next->element != value){
         tmp = tmp->next;	
     }
     return tmp;
 }	
 
-void delete_element(List L, ElementType value){
+void list_delete_element(List L, ElementType value){
     Position pre, tmp;
-    pre = find_previous(L,value);	
+    pre = list_find_previous(L,value);	
 
-    if(!is_last(L, pre)) {
-        tmp = pre->next;			//	The current value
+    if(!list_is_last(L, pre)) {
+        tmp = pre->next;			// The current value
         pre->next = tmp->next;
         free(tmp);
     }
 }
 
-void insert_list(List L, Position p, ElementType value){
+void list_insert(List L, Position p, ElementType value){
     Position tmp;
     tmp = (Position)malloc(sizeof(struct Node));
     if(!tmp) {
@@ -50,4 +77,25 @@ void insert_list(List L, Position p, ElementType value){
     tmp->element = value;
     tmp->next = p->next;
     p->next = tmp;
+}
+
+void list_clear(List L){
+    Position p, tmp;
+    p = L->next;
+    L->next=NULL;		//	
+
+    while(p != NULL){	
+        tmp = p->next;
+        free(p);
+        p = tmp;
+	}
+}
+
+Position list_header(List L){
+    return L;
+}
+
+Position list_first(List L){
+    if(L->next != NULL)
+		return L->next;
 }
--- data_structures/C03/list/single_list/list.h
+++ data_structures/C03/list/single_list/list.h
@@ -7,36 +7,45 @@ typedef struct Node *PtrToNode;
 typedef PtrToNode	List;
 typedef PtrToNode	Position;
 
-/* Make a list empty */
-List make_empty(List L);
-
 /** 
  * return true if L empty 
  **/
-bool is_empty(List L);
+bool list_is_empty(List L);
 
 /* return true if p is the last position in list L */
-bool is_last(Position p, List L);
+bool list_is_last(Position p, List L);
+
+/* Create a list form head */
+void list_create_form_head(List L, ElementType array[], int n);
+
+/* Create a list form head */
+void list_create_form_tail(List L, ElementType array[], int n);
 
 /* Find the element */
-Position find_elment(List L, ElementType value);
+Position list_find_elment(List L, ElementType value);
 
-void delete_element(List L, ElementType value);
+/* Find a value of previous */
+Position list_find_previous(List L, ElementType value);
 
-/**/
-Position find_previous(List L, ElementType value);
+/* Delete a exist element in List */
+void list_delete_element(List L, ElementType value);
 
-/*Insert vlaue to the list L*/
-void insert_list(List L, Position p, ElementType value);
+/* Insert  a element to the list L*/
+void list_insert(List L, Position p, ElementType value);
 
-void delete_list(List L);
+/* Clear the list of L */
+void list_clear(List L);
 
-Position header(List L);
-Position first(List L);
-Position advace(List L);
+/* Get the head list of L */
+Position list_header(List L);
 
-ElementType Retrieve(Position p);
+/* Get the first element position*/
+Position list_first(List L);
 
+/* */
+Position list_advace(List L);
+
+ElementType list_etrieve(Position p);
 
 /*list abs*/
 struct Node{
