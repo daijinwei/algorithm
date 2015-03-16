a759e5aa50ffade41762ddf9162c162b6541f191
daijinwei <daijinwei41@gmail.com>
2015-03-16 10:22:36 +0800

    Implement trie, add trie_tree.h file

---
+++ data_structures/C04/tree/trie_tree/trie_tree.h
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: trie_tree.h
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 16 Mar 2015 10:14:38 AM CST
+ ************************************************************************/
+
+#ifndef _TRIE_TREE_H
+#define _TRIE_TREE_H
+
+#define MAX_STR_NUM     26  // trie tree hax max 26 child
+
+struct TrieNode;
+typedef struct TrieNode *trie;
+    
+struct TrieNode{
+    struct TrieNode *next[MAX_STR_NUM];
+};
+
+#endif
